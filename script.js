9d7085f41c4217d9721a515929ee50417503b955
dev-andrewm <51978220+dev-andrewm@users.noreply.github.com>
2022-11-25 13:35:42 -0300

    refactoring

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const shareButton = document.querySelector('.share-button-box');
-const shareInfoContainer = document.querySelector('.share-icons-box');
+const shareInfoBox = document.querySelector('.share-icons-box');
 const arrowDown = document.querySelector('.arrow-down');
 
 shareButton.addEventListener('click', () => {
-    shareInfoContainer.classList.toggle('visible');
+    shareInfoBox.classList.toggle('visible');
     shareButton.classList.toggle('ontop');
     arrowDown.classList.toggle('arrowVisible');
 });
